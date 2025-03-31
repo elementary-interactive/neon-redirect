5b680c28789808af2b748378d7a8aacbbfe2760e
Egon <egonstefan@pm.me>
2025-03-31 16:00:21 +0200

    Remove, vendor and lock files

--- admin/Resources/RedirectResource.php
+++ admin/Resources/RedirectResource.php
@@ -3,100 +3,104 @@
 namespace Neon\Admin\Resources;
 
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Filament\Resources\Resource;
-use Neon\Redirect\Models\Redirect;
-use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
-use Neon\Admin\Resources\Traits\NeonAdmin;
 use Neon\Admin\Resources\RedirectResource\Pages;
+use Neon\Admin\Resources\Traits\NeonAdmin;
+use Neon\Redirect\Models\Redirect;
 
 class RedirectResource extends Resource
 {
-  use NeonAdmin;
+	use NeonAdmin;
 
-  protected static ?int $navigationSort = 6;
+	protected static ?int $navigationSort = 6;
 
-  protected static ?string $model = Redirect::class;
+	protected static ?string $model = Redirect::class;
 
-  protected static ?string $navigationIcon = 'heroicon-o-arrow-path';
+	protected static ?string $navigationIcon = 'heroicon-o-arrow-path';
 
-  protected static ?string $activeNavigationIcon = 'heroicon-s-arrow-path';
+	protected static ?string $activeNavigationIcon = 'heroicon-s-arrow-path';
 
-  protected static ?string $recordTitleAttribute = 'from';
+	protected static ?string $recordTitleAttribute = 'from';
 
-  public static function getNavigationLabel(): string
-  {
-    return __('neon-admin::admin.navigation.redirect');
-  }
+	public static function getNavigationLabel(): string
+	{
+		return __('neon-admin::admin.navigation.redirect');
+	}
 
-  public static function getNavigationGroup(): string
-  {
-    return __('neon-admin::admin.navigation.web');
-  }
+	public static function getNavigationGroup(): string
+	{
+		return __('neon-admin::admin.navigation.web');
+	}
 
-  public static function getModelLabel(): string
-  {
-    return __('neon-admin::admin.models.redirect');
-  }
+	public static function getModelLabel(): string
+	{
+		return __('neon-admin::admin.models.redirect');
+	}
 
-  public static function getPluralModelLabel(): string
-  {
-    return __('neon-admin::admin.models.redirects');
-  }
+	public static function getPluralModelLabel(): string
+	{
+		return __('neon-admin::admin.models.redirects');
+	}
 
-  public static function items(): array
-  {
-    $t = [
-      Forms\Components\TextInput::make('from')
-        ->required()
-        ->label(__('neon-admin::admin.resources.redirect.form.fields.from.label')),
-	Forms\Components\TextInput::make('to')
-        ->required()
-        ->label(__('neon-admin::admin.resources.redirect.form.fields.to.label')),
-    ];
+	public static function items(): array
+	{
+		$t = [
+			Forms\Components\TextInput::make('from')
+				->required()
+				->label(__('neon-admin::admin.resources.redirect.form.fields.from.label')),
+			Forms\Components\TextInput::make('to')
+				->required()
+				->label(__('neon-admin::admin.resources.redirect.form.fields.to.label')),
+		];
 
-    return $t;
-  }
+		return $t;
+	}
 
-  public static function table(Table $table): Table
-  {
-    return $table
-      ->columns([
-        Tables\Columns\TextColumn::make('from')
-          ->label(__('neon-admin::admin.resources.redirect.form.fields.from.label'))
-          ->searchable()
-          ->sortable()
-          ->toggleable(isToggledHiddenByDefault: false),
-		Tables\Columns\TextColumn::make('to')
-          ->label(__('neon-admin::admin.resources.redirect.form.fields.to.label'))
-          ->searchable()
-          ->sortable()
-          ->toggleable(isToggledHiddenByDefault: false),
-      ])
-      ->actions([
-        Tables\Actions\EditAction::make()
-          ->slideOver(),
-        Tables\Actions\DeleteAction::make(),
-        Tables\Actions\ForceDeleteAction::make(),
-        Tables\Actions\RestoreAction::make(),
-      ])
-      ->bulkActions(self::bulkActions())
-      ->defaultSort('created_at')
-      ->paginatedWhileReordering();
-  }
+	public static function table(Table $table): Table
+	{
+		return $table
+			->columns(
+				[
+					Tables\Columns\TextColumn::make('from')
+						->label(__('neon-admin::admin.resources.redirect.form.fields.from.label'))
+						->searchable()
+						->sortable()
+						->toggleable(isToggledHiddenByDefault: false),
+					Tables\Columns\TextColumn::make('to')
+						->label(__('neon-admin::admin.resources.redirect.form.fields.to.label'))
+						->searchable()
+						->sortable()
+						->toggleable(isToggledHiddenByDefault: false),
+				]
+			)
+			->actions(
+				[
+					Tables\Actions\EditAction::make()
+						->slideOver(),
+					Tables\Actions\DeleteAction::make(),
+					Tables\Actions\ForceDeleteAction::make(),
+					Tables\Actions\RestoreAction::make(),
+				]
+			)
+			->bulkActions(self::bulkActions())
+			->defaultSort('created_at')
+			->paginatedWhileReordering();
+	}
 
-  public static function getPages(): array
-  {
-    return [
-      'index' => Pages\ManageRedirects::route('/'),
-    ];
-  }
+	public static function getPages(): array
+	{
+		return [
+			'index' => Pages\ManageRedirects::route('/'),
+		];
+	}
 
-  public static function getEloquentQuery(): Builder
-  {
-    return parent::getEloquentQuery()
-      ->withoutGlobalScopes();
-  }
+	public static function getEloquentQuery(): Builder
+	{
+		return parent::getEloquentQuery()
+			->withoutGlobalScopes();
+	}
 }
