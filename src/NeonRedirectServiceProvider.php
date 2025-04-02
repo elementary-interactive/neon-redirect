9100e202096a3e8904783b0c6fa952350274831f
Egon <egonstefan@pm.me>
2025-04-02 14:14:51 +0200

    fix

--- src/NeonRedirectServiceProvider.php
+++ src/NeonRedirectServiceProvider.php
@@ -8,14 +8,15 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class NeonRedirectServiceProvider extends PackageServiceProvider
 {
-  const VERSION = '1.0.0-alpha-1';
+	const VERSION = '1.0.0-alpha-1';
 
-  public function configurePackage(Package $package): void
-  {
-    AboutCommand::add('N30N', 'Redirect', self::VERSION);
+	public function configurePackage(Package $package): void
+	{
+		AboutCommand::add('N30N', 'Redirect', self::VERSION);
 
-    $package
-      ->name('neon-redirect')
-      ->hasMigration('create_redirects_table');
-  }
+		$package
+			->name('neon-redirect')
+			->hasMigration('create_redirects_table')
+			->runsMigrations();
+	}
 }
